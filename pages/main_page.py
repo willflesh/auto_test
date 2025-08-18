e9af376d848f9f197d2ecddf39a4d8f362c4d56b
willflesh <vladismukhin@mail.ru>
2025-08-18 16:41:19 +1000

    изменения по проверке элемента на странице

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,3 +6,15 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
