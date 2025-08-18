077c95d98dfe292b4ffde0b07874912fdeba9cc3
willflesh <vladismukhin@mail.ru>
2025-08-18 16:41:43 +1000

    проверка элемента на странице

--- pages/base_page.py
+++ pages/base_page.py
@@ -12,3 +12,8 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
