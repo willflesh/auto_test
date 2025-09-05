ac18cda07eb5a461bcc59890d510e7f2ac021793
willflesh <vladismukhin@mail.ru>
2025-09-05 14:52:46 +1000

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 
 
 
+
 def pytest_addoption(parser):
     parser.addoption('--language', action="store", default="en",
                      help="Choose language: ru, en, fr, ..")
@@ -11,7 +12,16 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+    from selenium.webdriver.chrome.options import Options
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--incognito") 
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(10)
     yield browser
     print("\nquit browser..")
     browser.quit()
