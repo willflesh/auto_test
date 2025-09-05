6b641ed0ccaa4e68fc673a65a355df7268f99c00
willflesh <vladismukhin@mail.ru>
2025-09-05 14:20:24 +1000

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium import webdriver
+import tempfile
+import shutil
 
 
 def pytest_addoption(parser):
@@ -10,7 +12,14 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+    options = ChromeOptions()
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    user_data_dir=tempfile.mkdtemp()
+    options.add_arguments(f"--user-data-dir={user_data_dir}")
+    browser=webrdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
+    shutil.rmtree(user_data_dir)
